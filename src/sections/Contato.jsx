e6269690c51c1822bf7e29dad859f3591c972dad
Júlia Bacchi <134342345+juliabacchi1@users.noreply.github.com>
2025-05-15 10:58:42 -0300

    Incluídos icons do iconoir-react

--- src/sections/Contato.jsx
+++ src/sections/Contato.jsx
@@ -1,3 +1,5 @@
+import { Mail, Github, Linkedin, Phone } from "iconoir-react";
+
 export default function Contato() {
   return (
     <section
@@ -14,56 +16,48 @@ export default function Contato() {
       <h2 className="font-serif text-[85px] text-[#156669] text-center">
         Me chama
       </h2>
-      <p className="text-[36px] text-center text-gray-800 mb-10">
+      <p className="text-[36px] text-center text-gray-800">
         Vamos trabalhar juntos
       </p>
 
-      <div className="flex gap-8 mb-16">
+      <div className="flex gap-6 m-7">
         <a
-          href="https://wa.me/SEUNUMERO"
+          href="https://wa.me/48991779743?text=Oi%20"
           target="_blank"
           rel="noopener noreferrer"
+          className="hover:scale-110 transition-transform"
         >
-          <img
-            src="/whatsapp.svg"
-            alt="WhatsApp"
-            className="w-10 h-10 hover:scale-110 transition"
-          />
+          <Phone className="w-8 h-8 text-[#1F2937]" />
         </a>
         <a
-          href="https://github.com/seuusuario"
+          href="https://github.com/juliabacchi1"
           target="_blank"
           rel="noopener noreferrer"
+          className="hover:scale-110 transition-transform"
         >
-          <img
-            src="/github.svg"
-            alt="GitHub"
-            className="w-10 h-10 hover:scale-110 transition"
-          />
+          <Github className="w-8 h-8 text-[#1F2937]" />
         </a>
         <a
-          href="https://linkedin.com/in/seuusuario"
+          href="https://www.linkedin.com/in/juliabacchi/"
           target="_blank"
           rel="noopener noreferrer"
+          className="hover:scale-110 transition-transform"
         >
-          <img
-            src="/linkedin.svg"
-            alt="LinkedIn"
-            className="w-10 h-10 hover:scale-110 transition"
-          />
+          <Linkedin className="w-8 h-8 text-[#1F2937]" />
         </a>
-        <a href="mailto:seuemail@email.com">
-          <img
-            src="/email.svg"
-            alt="Email"
-            className="w-10 h-10 hover:scale-110 transition"
-          />
+        <a
+          href="mailto:juliabacchi92@gmail.com"
+          className="hover:scale-110 transition-transform"
+        >
+          <Mail className="w-8 h-8 text-[#1F2937]" />
         </a>
       </div>
 
-      <button className="bg-[#EAE0D4] font-semibold px-9 py-3 rounded-full shadow hover:bg-white">
-        blog tech
-      </button>
+      <div className="mt-10">
+        <button className="bg-[#EAE0D4] font-semibold px-9 py-3 rounded-full shadow hover:bg-white">
+          blog tech
+        </button>
+      </div>
     </section>
   );
 }
