e7de906845f4a28ebf62b832e9a23cca94940f18
Júlia Bacchi <134342345+juliabacchi1@users.noreply.github.com>
2025-05-15 00:05:02 -0300

    Projetos adicionados

--- src/sections/Works.jsx
+++ src/sections/Works.jsx
@@ -1,47 +1,122 @@
 export default function Works() {
   return (
-    <section
-      id="works"
-      className="h-screen relative bg-white flex flex-row justify-between items-center"
-    >
-      <div className="absolute top-0 left-0 px-6 py-6 z-10">
-        <a href="#home" className="flex items-center">
-          <img className="w-10 h-10 mr-2" src="logo.svg" alt="logo" />
-        </a>
-      </div>
-
-      <div className="w-[40%] mx-10 flex flex-col justify-center items-start text-left">
-        <h2 className="text-[67px] px-10 mb-4">Trabalhos</h2>
-        <p className="text-[16.5px] px-10 text-gray-700 mb-8 max-w-md">
-          Alguns dos meus projetos <br /> entregues e apps white label.
-        </p>
-        <div className="flex gap-4 px-10">
-          <button className="bg-[#F6F7EC] font-semibold px-7 py-3 rounded-full shadow hover:bg-[#EAE0D4]">
-            me chama
-          </button>
-          <button className="bg-[#EAE0D4] font-regular px-8 py-3 rounded-full shadow hover:bg-[#F6F7EC]">
-            blog tech
-          </button>
+    <>
+      <section
+        id="works"
+        className="h-screen relative bg-white flex flex-row justify-between items-center"
+      >
+        <div className="absolute top-0 left-0 px-6 py-6 z-10">
+          <a href="#home" className="flex items-center">
+            <img className="w-10 h-10 mr-2" src="logo.svg" alt="logo" />
+          </a>
         </div>
-      </div>
 
-      {/* Parte do meio - Imagem */}
-      <div className="w-[20%] flex justify-center items-center">
-        <img
-          src="/ecotasks-d.png"
-          alt="Descrição ecotasks"
-          className="max-w-full h-auto object-contain"
-        />
-      </div>
+        <div className="w-[40%] mx-10 flex flex-col justify-center items-start text-left">
+          <h2 className="text-[67px] px-10 mb-4">Trabalhos</h2>
+          <p className="text-[16.5px] px-10 text-gray-700 mb-8 max-w-md">
+            Alguns dos meus projetos <br /> entregues e apps white label.
+          </p>
+          <div className="flex gap-4 px-10">
+            <button className="bg-[#F6F7EC] font-semibold px-7 py-3 rounded-full shadow hover:bg-[#EAE0D4]">
+              me chama
+            </button>
+            <button className="bg-[#EAE0D4] font-regular px-8 py-3 rounded-full shadow hover:bg-[#F6F7EC]">
+              blog tech
+            </button>
+          </div>
+          <div className="absolute bottom-8 left-20 z-10">
+            <a href="#next-project" className="flex items-center">
+              <button className="w-12 h-12 rounded-full bg-[#cbb396] flex items-center justify-center shadow hover:bg-[#b8a276]">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6 text-white"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M19 9l-7 7-7-7"
+                  />
+                </svg>
+              </button>
+            </a>
+          </div>
+        </div>
 
-      {/* Parte da direita - Imagem */}
-      <div className="w-[40%] flex justify-center items-center">
-        <img
-          src="/projeto2.png"
-          alt="Projeto 2"
-          className="max-w-full h-auto object-contain"
-        />
-      </div>
-    </section>
+        <div className="w-[20%] flex justify-center items-center">
+          <a href="https://github.com/juliabacchi1/ecotasks" target="_blank">
+            <img
+              src="/ecotasks-d.png"
+              alt="Descrição ecotasks"
+              className="max-w-full h-auto object-contain"
+            />
+          </a>
+        </div>
+
+        <div className="w-[40%] flex justify-center items-center">
+          <a href="https://ecotasks.vercel.app/" target="_blank">
+            <img
+              src="/ecotasks-v.png"
+              alt="Visualização ecotasks"
+              className="max-w-[80%] h-auto object-contain translate-y-10"
+            />
+          </a>
+        </div>
+      </section>
+
+      <section
+        id="next-project"
+        className="h-screen relative bg-white flex flex-row justify-between items-center"
+      >
+        <div className="absolute top-0 left-0 px-6 py-6 z-10">
+          <a href="#home" className="flex items-center">
+            <img className="w-10 h-10 mr-2" src="logo.svg" alt="logo" />
+          </a>
+        </div>
+        <div className="absolute bottom-8 left-20 z-10">
+          <a href="#next-project" className="flex items-center">
+            <button className="w-12 h-12 rounded-full bg-[#cbb396] flex items-center justify-center shadow hover:bg-[#b8a276]">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6 text-white"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M19 9l-7 7-7-7"
+                />
+              </svg>
+            </button>
+          </a>
+        </div>
+
+        <div className="flex">
+          <div className="w-[50%] flex justify-end items-center">
+            <img
+              src="/rota-d.png"
+              alt="Descrição Baleia Franca"
+              className="max-w-[40%] h-auto object-contain"
+            />
+          </div>
+
+          <div className="w-[40%] flex justify-start items-center">
+            <a href="https://rotabaleiafranca.com.br/" target="_blank">
+              <img
+                src="/rota-v.png"
+                alt="Visualização Baleia Franca"
+                className="max-w-[80%] h-auto object-contain translate-y-10"
+              />
+            </a>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
