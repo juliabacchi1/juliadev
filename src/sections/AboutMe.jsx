468441807ae5eb047609ff6523475c9049b8ff4a
Júlia Bacchi <134342345+juliabacchi1@users.noreply.github.com>
2025-05-15 23:51:15 -0300

    Ajustes no AboutMe.jsx e Techs.jsx config mobile

--- src/sections/AboutMe.jsx
+++ src/sections/AboutMe.jsx
@@ -2,7 +2,7 @@ export default function AboutMe() {
   return (
     <section
       id="sobre-mim"
-      className="relative flex flex-col md:flex-row h-screen"
+      className="relative flex flex-col md:flex-row h-auto md:h-screen"
     >
       <div className="absolute top-0 left-0 px-6 py-6 z-10">
         <a href="#home" className="flex items-center">
@@ -10,20 +10,20 @@ export default function AboutMe() {
         </a>
       </div>
 
-      <div className="w-full md:w-1/2 bg-[#FCF5EC] flex justify-center items-center">
+      <div className="w-full md:w-1/2 bg-[#FCF5EC] flex justify-center items-center py-10 md:py-0">
         <img
           src="foto.png"
           alt="Júlia Bacchi"
-          className="h-[80%] object-contain"
+          className="h-72 md:h-[80%] object-contain"
         />
       </div>
 
-      <div className="w-full md:w-1/2 bg-[#EEA595] flex flex-col justify-center items-start text-left px-10">
-        <div className="p-8 mx-8 text-start">
-          <h2 className="font-serif text-[#BA4B2E] text-6xl md:text-6xl mb-6 px-8 leading-snug">
-            Olá, seja <br /> bem-vindo
+      <div className="w-full md:w-1/2 bg-[#EEA595] flex flex-col justify-center items-start py-8">
+        <div className="p-2 md:p-8 mx-8 text-start">
+          <h2 className="font-serif text-[#BA4B2E] text-2xl md:text-6xl mb-6 px-8 leading-snug text-center md:text-left">
+            Olá, seja <br className="hidden md:block" /> bem-vindo
           </h2>
-          <p className="text-base md:text-[16.5px] text-gray-800 mb-8 px-8">
+          <p className="text-base text-[13px] md:text-[16.5px] text-gray-800 mb-8 md:px-8 text-center md:text-left">
             Sou Júlia Bacchi, desenvolvedora front-end, libriana, mãe de pet e
             apaixonada por inovação digital. Acredito que a tecnologia deve ser
             intuitiva, impactante e uma força para o bem.
@@ -32,13 +32,13 @@ export default function AboutMe() {
             realmente deve ser.
           </p>
 
-          <div className="flex gap-4 px-8">
-            <a href="#techs" className="flex items-center">
+          <div className="flex flex-wrap justify-center md:justify-start gap-4 md:px-8">
+            <a href="#techs">
               <button className="bg-white text-[#BA4B2E] font-semibold px-5 py-3 rounded-full shadow hover:bg-[#BA4B2E]/50 hover:text-white">
                 tecnologias
               </button>
             </a>
-            <a href="#contato" className="flex items-center">
+            <a href="#contato">
               <button className="bg-[#BA4B2E]/50 text-white font-regular px-7 py-3 rounded-full shadow hover:bg-white hover:text-[#BA4B2E]">
                 me chama
               </button>
--- src/sections/Techs.jsx
+++ src/sections/Techs.jsx
@@ -2,60 +2,60 @@ export default function Techs() {
   return (
     <section
       id="techs"
-      className="h-screen relative bg-cover bg-center flex flex-col justify-between"
+      className="h-auto md:h-screen relative bg-cover bg-center flex flex-col justify-between"
       style={{ backgroundImage: "url('/nuvem.png')" }}
     >
-      <div className="absolute top-0 left-0 px-6 py-6 z-10">
+      <div className="absolute top-0 left-0 px-4 md:px-6 py-4 md:py-6 z-10">
         <a href="#home" className="flex items-center">
           <img className="w-10 h-10 mr-2" src="logo.svg" alt="logo" />
         </a>
       </div>
 
-      <div className="px-10 mt-10 ml-20">
-        <h2 className="text-[67px]">Tecnologias</h2>
+      <div className="px-4 md:px-10 mt-20 md:mt-10 ml-0 md:ml-20 text-center md:text-left">
+        <h2 className="text-[36px] md:text-[67px] mt-6 md:mt-0">Tecnologias</h2>
       </div>
 
-      <div className="flex flex-col items-center">
-        <div className="grid grid-cols-2 md:grid-cols-4 gap-6">
+      <div className="flex flex-col items-center mt-10 md:mt-0">
+        <div className="grid grid-cols-3 md:grid-cols-4 gap-6 px-4 md:px-0">
           <img
             src="/react.png"
             alt="React"
-            className="w-40 h-40 rounded-full hover:scale-105 transition-transform duration-300 hover:brightness-110"
+            className="w-24 h-24 md:w-40 md:h-40 rounded-full hover:scale-105 transition-transform duration-300 hover:brightness-110"
           />
           <img
             src="/vite.png"
             alt="Vite"
-            className="w-40 h-40 rounded-full hover:scale-105 transition-transform duration-300 hover:brightness-110"
+            className="w-24 h-24 md:w-40 md:h-40 rounded-full hover:scale-105 transition-transform duration-300 hover:brightness-110"
           />
           <img
             src="/nextjs.png"
             alt="Next.js"
-            className="w-40 h-40 rounded-full hover:scale-105 transition-transform duration-300 hover:brightness-110"
+            className="w-24 h-24 md:w-40 md:h-40 rounded-full hover:scale-105 transition-transform duration-300 hover:brightness-110"
           />
           <img
             src="/figma.png"
             alt="Figma"
-            className="w-40 h-40 rounded-full hover:scale-105 transition-transform duration-300 hover:brightness-110"
+            className="w-24 h-24 md:w-40 md:h-40 rounded-full hover:scale-105 transition-transform duration-300 hover:brightness-110"
           />
           <img
             src="/tailwind.png"
             alt="Tailwind"
-            className="w-40 h-40 rounded-full hover:scale-105 transition-transform duration-300 hover:brightness-110"
+            className="w-24 h-24 md:w-40 md:h-40 rounded-full hover:scale-105 transition-transform duration-300 hover:brightness-110"
           />
           <img
             src="/javascript.png"
             alt="JavaScript"
-            className="w-40 h-40 rounded-full hover:scale-105 transition-transform duration-300 hover:brightness-110"
+            className="w-24 h-24 md:w-40 md:h-40 rounded-full hover:scale-105 transition-transform duration-300 hover:brightness-110"
           />
           <img
             src="/typescript.png"
             alt="TypeScript"
-            className="w-40 h-40 rounded-full hover:scale-105 transition-transform duration-300 hover:brightness-110"
+            className="w-24 h-24 md:w-40 md:h-40 rounded-full hover:scale-105 transition-transform duration-300 hover:brightness-110"
           />
         </div>
       </div>
 
-      <div className="flex justify-center pb-10">
+      <div className="flex justify-center pb-10 mt-10 md:mt-0">
         <a href="#works" className="flex items-center">
           <button className="bg-white font-semibold px-9 py-3 rounded-full shadow hover:bg-gray-100">
             trabalhos
