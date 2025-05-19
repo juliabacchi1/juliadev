7cd488d2914528574a555f10e0dd80ddbb0de55c
Júlia Bacchi <134342345+juliabacchi1@users.noreply.github.com>
2025-05-19 15:39:26 -0300

    Imagem para page not founde

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -2,24 +2,24 @@ import NotFoundImg from "../assets/404-image.png";
 
 export default function NotFound() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center text-center p-6">
+    <div className="min-h-screen bg-[#8c82b3] flex flex-col items-center justify-center text-center p-6">
       <img
         src={NotFoundImg}
         alt="Erro 404 - Página não encontrada"
-        className="w-60 md:w-96 mb-6"
+        className="w-60 md:w-72 h-auto mb-10"
       />
 
-      <h1 className="text-3xl md:text-5xl font-bold mb-4 text-gray-800">
+      <h1 className="text-3xl md:text-5xl font-semibold mb-4 text-white">
         Opa! Página não encontrada
       </h1>
 
-      <p className="mb-6 text-gray-600 text-base md:text-lg">
+      <p className="mb-6 text-white text-base md:text-lg">
         A página que você tentou acessar não existe ou foi movida.
       </p>
 
       <a
         href="/"
-        className="bg-black text-white px-6 py-3 rounded-full shadow hover:bg-gray-800 transition"
+        className="bg-white text-[#8c82b3] font-semibold px-6 py-3 rounded-full shadow hover:bg-gray-100 transition"
       >
         Voltar para o início
       </a>
